e3f22b38d2fad421e1f04918bf231910b8278083
AAVolkov <AAVolkov@synergy.ru>
2017-09-06 19:00:19 +0300

    Скелет роутинга

---
+++ application/controllers/DefaultPageController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @link http://www.php-devil.ru/
+ * @copyright Copyright (c) 2017 Web Wizardry
+ * @license http://www.php-devil.ru/license/
+ */
+
+namespace app\controllers;
+use app\components\DefaultFrontendController;
+
+class DefaultPageController extends DefaultFrontendController
+{
+
+}
