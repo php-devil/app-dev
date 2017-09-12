5187b93d4e4376e2b34a4b6ea530b08505604980
Алексей Волков <avolkov.webwizardry@gmail.com>
2017-09-12 22:39:06 +0300

    Отображение страниц по умолчанию

--- application/controllers/DefaultPageController.php
+++ application/controllers/DefaultPageController.php
@@ -8,7 +8,17 @@
 namespace app\controllers;
 use app\components\DefaultFrontendController;
 
+/**
+ * Class DefaultPageController
+ * Контроллер для отображения страниц по умолчанию
+ * 
+ * @package app\controllers
+ * @author Alexey Volkov <alex.phpdevil@gmail.com>
+ */
 class DefaultPageController extends DefaultFrontendController
 {
+    public function actionIndexPage()
+    {
 
+    }
 }
